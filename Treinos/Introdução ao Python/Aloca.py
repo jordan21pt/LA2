a0ff72aa76020f4cdc49cf8890a5a29e48889681
Rui Jordão Sampaio Gonçalves <58782713+jordan21pt@users.noreply.github.com>
2023-02-20 14:59:51 +0000

    Update Aloca.py

--- Treinos/Introdução ao Python/Aloca.py
+++ Treinos/Introdução ao Python/Aloca.py
@@ -8,22 +8,31 @@ por um aluno. A função deve devolver a lista com os alunos que não ficaram
 alocados a nenhum projecto, ordenada por ordem de número de aluno.
 
 """
+"""
+
+Implemente uma função que dado um dicionário com as preferências dos alunos
+por projectos (para cada aluno uma lista de identificadores de projecto, 
+por ordem de preferência), aloca esses alunos aos projectos. A alocação
+é feita por ordem de número de aluno, e cada projecto só pode ser feito
+por um aluno. A função deve devolver a lista com os alunos que não ficaram
+alocados a nenhum projecto, ordenada por ordem de número de aluno.
+
+"""
 
 def aloca(prefs):
     
-    final = [key for key in prefs] 
-    final.sort()
+    final = sorted(prefs.keys()) 
+
     
     new_dict = {}
     n_alocados = []
     
     for aluno in final:
-        if prefs.get(aluno):
-            for a in prefs.get(aluno):
-                if a not in new_dict:
-                    new_dict.update({a: aluno})
-                    break
-            else:
-                n_alocados.append(aluno)
+        for a in prefs.get(aluno):
+            if a not in new_dict:
+                new_dict.update({a: aluno})
+                break
+        else:
+            n_alocados.append(aluno)
                 
     return n_alocados
